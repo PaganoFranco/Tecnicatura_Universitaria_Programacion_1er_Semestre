1744a32e4e819771279764a25833d877a9f287c3
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-03 19:19:43 -0300

    Clase 3: Tipos de datos en Python

--- Python/Leccion 2/main.py
+++ Python/Leccion 2/main.py
@@ -42,3 +42,41 @@ print(type(a))
 # Manejo de cadenas (String)
 miGrupoFavorito = "Guns and Roses"
 print("Mi grupo favorito es: " + miGrupoFavorito)
+
+# Manejo de cadenas (String)
+miGrupoFavorito = "Guns and Roses"
+caracteristicas = "The best rock band"
+# Una forma de concatenar es con el "+"
+print("Mi grupo favorito es: " + miGrupoFavorito + " " + caracteristicas)
+# Otra forma de concatenar es usando ","
+print("Mi grupo favorito es:" , miGrupoFavorito , caracteristicas)
+# la direfencia es que al usar el signo "+", debemos agregar los espacios.
+# por otro lado, al usar la "," se asginan de forma automatica los espacios.
+
+numero1 = "7"
+numero2 = "8"
+print(numero1 + numero2)
+# De este modo, solo concatena las variables definidas como string
+print(int(numero1 + numero2))
+# al usar Int, convertimos de tipo string a tipo entero, siempre y cuando
+# el string sea un numero
+
+# Tipo Booleanos (Bool)
+miBooleano = 1 > 2
+print(miBooleano)
+# condicional
+if miBooleano:
+    print("El resultado es Verdadero")
+else:
+    print("El resultado es Falso")
+
+
+# Procesar la entrada del usuario
+resultado = input("Ingrese un numero: ") # Regresa un dato del tipo string
+print(resultado)
+
+# Conversion de la entrada de datos
+numero1 = int(input("Escribir el primer numer: "))
+numero2 = int(input("Escribir el segundo numero: "))
+resultado = numero1 + numero2
+print("la resultado de la suma es:", resultado)
