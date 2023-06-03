a3182a684c4a3f85dd0a918144b0d789045408d6
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-03 18:34:31 -0300

    Clase 2: Variables en Python

---
+++ Python/Leccion 2/main.py
@@ -0,0 +1,44 @@
+# Clase N° 2 - Variables
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los estudiantes de la tecnicatura"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+# Las literales se escibren x528, la variable y = x272, la variable z = 592
+print(id(y))
+print(id(z))
+
+# Clase N°3 - Tipos de datos Int, Float, String, Bool
+# tipo Int
+a = 10
+print(a)
+print(type(a))
+# tipo String
+a = "Hola estudiantes"
+print(a)
+print(type(a))
+# el comando type me escribe el tipo de dato que tiene la variable
+a: str = "Hola estudiantes"
+print(a)
+print(type(a))
+# En este caso se le agrega ": str" para referirse al tipo de dato, no lo condiciona
+# tipo flotante:
+a = 10.70
+print(a)
+print(type(a))
+# Tipo Booleano
+a = True
+print(a)
+print(type(a))
+a = False
+print(a)
+print(type(a))
+# Se escribren siempre con la primera en mayuscula
+
+# Manejo de cadenas (String)
+miGrupoFavorito = "Guns and Roses"
+print("Mi grupo favorito es: " + miGrupoFavorito)
