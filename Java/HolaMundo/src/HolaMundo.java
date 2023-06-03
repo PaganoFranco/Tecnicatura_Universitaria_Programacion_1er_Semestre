8529c6966020704d6fede145f52910e9d55b5587
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-03 20:05:57 -0300

    Clase 1: Fundamentos Java

---
+++ Java/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    //psvm + Tab (Cada una de las sigas del comando que sigue) 
+    public static void main(String[] args) {
+        //sout + Tab
+        System.out.println("Hola Mundo desde Java");     
+    }
+}
