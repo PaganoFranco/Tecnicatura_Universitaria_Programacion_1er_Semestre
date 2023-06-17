775a7553e2d7f06b5ef7f7b9257dd2ddc3e4a45c
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-17 14:19:11 -0300

    Clase 6: Sentencias de Control

--- Python/Leccion 4/main.py
+++ Python/Leccion 4/main.py
@@ -1,1 +1,24 @@
+# CLASE 6 - Sentencias de Control
+'''
+# Sentencia if/else
+condicion = True
+if condicion == True:
+    print("Condicion Verdader")
+elif condicion == False:
+    print("Condicion Falsa")
+else:
+    print("Condicion sin espesificar")
 
+# Ejercicio: Conversion de texto a numero
+num = int(input("Ingrese un numero en el rango (1 a 3): "))
+numTexto = ""
+if num == 1:
+    numTexto = "Numero uno"
+elif num == 2:
+    numTexto = "Numero dos"
+elif num == 3:
+    numTexto = "Numero tres"
+else:
+    numTexto = "Has ingresado un numero fuera del rango"
+print(f"El numero ingresado es: {num} - {numTexto}")
+'''
