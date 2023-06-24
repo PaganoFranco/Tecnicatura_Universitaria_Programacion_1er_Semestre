74e861ce2c45adf01dabb912c97db991220c4d0c
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-23 21:10:36 -0300

    Clase 7 - Operadores en Java Parte

--- Java/HolaMundo/src/HolaMundo.java
+++ Java/HolaMundo/src/HolaMundo.java
@@ -130,8 +130,55 @@ public class HolaMundo {
         System.out.println("El valor minimo de Double: "+ Double.MIN_VALUE);
         System.out.println("El valor maximo de Double: "+ Double.MAX_VALUE);
         
+        
+        //Clase 5
+        //Clase 6 
+        //EN NOTION
+        
+        //Clase 7
+        int num1 = 5, num2 = 4;
+        var solucion = num1 + num2;
+        System.out.println("solucion suma = " + solucion);
+        
+        solucion = num1 - num2;
+        System.out.println("solucion resta = " + solucion);
+        
+        solucion = num1 * num2;
+        System.out.println("solucion multriplicacion = " + solucion);
+        
+        solucion = num1 / num2;
+        System.out.println("solucion division = " + solucion);
+        
+        var solucion2 = 3.4 / num2;
+        System.out.println("solucion2 resultado de la division = " + solucion2);
+        
+        solucion = num1 % num2; //Guarda el residuo entero de la division.
+        System.out.println("solucion = " + solucion); // 5/4 
+        
+        if (num1 % 2 == 0)
+        System.out.println("Es un numero par");
+        else
+        System.out.println("Es un numero impar");
         */
         
+        //OPERADOR DE ASIGNACION
+        int varNum1 = 1, varNum2 = 4;
+        int varNum3 = varNum1 + 6 - varNum2; //Una operacion
+        System.out.println("varNum3 = " + varNum3);
         
+        varNum1 += 1; //
+        System.out.println("varNum1 = " + varNum1);
+        
+         varNum2 -= 2; //
+        System.out.println("varNum2 = " + varNum2);
+         
+        varNum1 *= 5; //
+        System.out.println("varNum1 = " + varNum1);
+        
+         varNum3 /= 4; //
+        System.out.println("varNum3 = " + varNum3);
+        
+         varNum1 %= 6; //
+        System.out.println("varNum1 = " + varNum1);
     }
 }
