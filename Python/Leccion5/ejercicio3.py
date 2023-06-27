230e5865a4a77ec1012d77c964abeed256b8d9ae
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-27 00:40:48 -0300

    Clase 7 - Ejercicio 4

--- Python/Leccion5/ejercicio3.py
+++ Python/Leccion5/ejercicio3.py
@@ -1,12 +1,29 @@
-num = 0
-while num < 1 or 12 < num:
-    num = int(input("Ingrese el numero del mes en el que se encuentra: "))
+# Ejercicio 3 - Calculadora estacion del año
+mes = int(input("Ingrese el numero del mes en el que se encuentra: "))
+estacion = None
+if 1 <= mes <= 3:
+    estacion = "Verano"
+elif 3 < mes <= 6:
+    estacion = "Otoño"
+elif 6 < mes <= 9:
+    estacion = "Invierno"
+elif 9 < mes <= 12:
+    estacion = "Primavera"
 else:
-    if 1 <= num <= 3 :
+    estacion = "no corresponde a un numero del mes"
+print(f"Para el mes Nª {mes}, corresponde a la estacion {estacion}")
+""" 
+# APLICANDO CICLO WHILE
+mes = int(input("Ingrese el numero del mes en el que se encuentra: "))
+while mes < 1 or 12 < mes:
+    mes = int(input("Ingrese el numero del mes en el que se encuentra: "))
+else:
+    if 1 <= mes <= 3 :
         print("Estas en la estacion de Verano")
-    elif 3 < num <= 6 :
+    elif 3 < mes <= 6 :
         print("Estas en la estacion de Otoño")
-    elif 6 < num <= 9 :
+    elif 6 < mes <= 9 :
         print("Estas en la estacion de Invierno")
-    elif 9 < num <= 12:
+    elif 9 < mes <= 12:
         print("Estas en la estacion de Primavera")
+"""
