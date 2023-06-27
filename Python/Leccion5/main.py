2fa0871899ead5dc05b8771b8561af5bb2663d0a
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-06-26 23:44:28 -0300

    Clase 7 - Ejercicio 3

--- Python/Leccion5/main.py
+++ Python/Leccion5/main.py
@@ -44,7 +44,7 @@ for letra in "Alemania":
         break
 else:
     print("Fin del ciclo for")
-"""
+
 # Palabra reservada continue
 for i in range(6):
     if i % 2 == 0:
@@ -54,3 +54,16 @@ for i in range(6):
     if i % 2 == 0:
         continue
     print(f"Valor: {i}")
+"""
+num = 0
+while num < 1 or 12 < num:
+    num = int(input("Ingrese el numero del mes en el que se encuentra: "))
+else:
+    if 1 <= num <= 3 :
+        print("Estas en la estacion de Verano")
+    elif 3 < num <= 6 :
+        print("Estas en la estacion de Otoño")
+    elif 6 < num <= 9 :
+        print("Estas en la estacion de Invierno")
+    elif 9 < num <= 12:
+        print("Estas en la estacion de Primavera")
