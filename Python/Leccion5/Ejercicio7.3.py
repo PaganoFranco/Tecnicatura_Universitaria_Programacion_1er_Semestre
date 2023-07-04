0efc64df62e098b645cb7723dbc2f016661a5484
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-07-04 01:31:22 -0300

    Clase 7.3

---
+++ Python/Leccion5/Ejercicio7.3.py
@@ -0,0 +1,9 @@
+"""
+Leer 10 numeros e imprimir cuantos son primos
+cuantos negativos y cuantos neutros
+"""
+with conteo != 10:
+    num = int(input("Ingrese un numero"))
+    if num == 0:
+        conteo_neutro +=1
+    elif: 
