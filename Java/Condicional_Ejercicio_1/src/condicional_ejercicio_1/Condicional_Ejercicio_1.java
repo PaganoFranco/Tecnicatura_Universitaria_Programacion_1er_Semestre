a9c6b28335972d8bbafb0f6af4a64982fe8012af
PAGANO,Franco <francopaganoo2015@gmail.com>
2023-07-05 21:16:59 -0300

    Clase 10 - Ejercicio2

--- Java/Condicional_Ejercicio_1/src/condicional_ejercicio_1/Condicional_Ejercicio_1.java
+++ Java/Condicional_Ejercicio_1/src/condicional_ejercicio_1/Condicional_Ejercicio_1.java
@@ -13,9 +13,14 @@ public class Condicional_Ejercicio_1 {
         if(mes == 1 || mes == 2 || mes == 3){
             estacion = "Verano";
         }
-        else {
-            System.out.println("estacion = " + estacion);
+         else if (mes == 4 || mes == 5 || mes == 6) {
+            estacion = "Otoño";
+        } else if (mes == 7 || mes == 8 || mes == 9) {
+            estacion = "Invierno";
+        } else if (mes == 10 || mes == 11 || mes == 12) {
+            estacion = "Primavera";
         }
+         System.out.println("estacion = " + estacion);
     }
 }
-        
+
